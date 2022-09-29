959a82dc676c815093676e099d188f11fdb5b659
yuchuan xue <yuchuanxue@yuchuandeMacBook-Pro.local>
2022-09-28 20:28:16 -0400

    modified Polynomial.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,46 @@
-public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+import java.io.File;
+import java.io.IOException;
+
+public class Driver {
+	public static void main(String [] args) { 
+		  Polynomial p = new Polynomial(); 
+		  System.out.println(p.evaluate(3)); 
+		  double [] c1 = {6, 5};
+		  int [] e1 = {0, 3};
+		  Polynomial p1 = new Polynomial(c1, e1); 
+		  double [] c2 = {-2,-9}; 
+		  int [] e2 = {1, 4};
+		  Polynomial p2 = new Polynomial(c2, e2); 
+		  Polynomial s = p1.add(p2); 
+		  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+		  if(s.hasRoot(1)) 
+		   System.out.println("1 is a root of s"); 
+		  else 
+		   System.out.println("1 is not a root of s"); 
+		  Polynomial m = p1.multiply(p2);
+		  System.out.println("m(0) = " + m.evaluate(0));
+		  System.out.println("m(1) = " + m.evaluate(1));
+		  
+		  try {
+		      File myObj = new File("myFile.txt");
+		      if (myObj.createNewFile()) {
+		    	  System.out.println("File created: " + myObj.getName());
+		      } else {
+		    	  System.out.println("Already exists.");
+		      }
+		    } catch (IOException e) {
+		      System.out.println("Error!");
+		      e.printStackTrace();
+		  }
+		  
+		  s.saveToFile("myFile.txt");
+		  File myObj = new File("myFile.txt");
+		  Polynomial fs = new Polynomial(myObj);
+		  System.out.println("fs(0.1) = " + fs.evaluate(0.1)); 
+		  
+		  p2.saveToFile("myFile.txt");
+		  File myObj2 = new File("myFile.txt");
+		  Polynomial fp2 = new Polynomial(myObj2);
+		  System.out.println("fp2(-1) = " + fp2.evaluate(-1)); 
+		 } 
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,39 +1,145 @@
-public class Polynomial{
-	double [] arr;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Polynomial {
+	double [] co;
+	int [] ex;
     
     public Polynomial(){
     }
     
-    public Polynomial (double[] a){
-    	arr = a;
-    }
-    
-    public Polynomial add(Polynomial b){
-    	int al = arr.length;
-    	Polynomial copy = new Polynomial(arr);
-    	int bl = (b.arr).length;
-    	if (al >= bl){
-    		for(int i=0; i<bl; i++){
-    			(copy.arr)[i] += (b.arr)[i];
-    		}
-    		return copy;
-        }
+    public Polynomial (double[] a, int[] b){
+    	if (a.length == b.length) {
+        	co = a;
+        	ex = b;
+    	}
     	else {
-    		for(int j=0; j<al; j++){
-    			(b.arr)[j] += (copy.arr)[j];
-    		}
-    		return b;
+    		System.out.println("Invalid input");
     	}
     }
     
+    public Polynomial(File f){
+    	try {
+    		Scanner input = new Scanner(f);
+        	while(input.hasNextLine()) {
+        		String line = input.nextLine();
+        		String [] str = line.split("X");
+        		int a = str.length;
+        	    double [] strco = new double[a];
+        	    int [] strex = new int[a];
+        	    int bo = 1;
+        	    	
+        	    if(str[0].indexOf('+')!=-1) {
+        	    	String [] str1 = str[0].split("+");
+        	    	strco[0] = Double.parseDouble(str1[0]);
+        	    	strex[0] = 0;
+        	    	strco[1] = Double.parseDouble(str1[1]);
+        	    }
+        	    else if((str[0].indexOf('-')!=-1)&&(str[0].indexOf('-')!=0)) {
+        	    	String [] str1 = str[0].split("-");
+        	    	strco[0] = Double.parseDouble(str1[0]);
+        	    	strex[0] = 0;
+        	    	strco[1] = Double.parseDouble("-"+str1[1]);
+        	    }
+        	    else {
+        	    	strco[0] = Double.parseDouble(str[0]);
+        	    	bo = 0;
+        	    }
+        	    if(bo==1) {
+        	    	for(int i=1; i<a-1; i++) {
+        	    		if(str[i].indexOf('+')!=-1) {
+        	    			String [] strr = str[i].split("\\+");
+        	    			strex[i] = Integer.parseInt(strr[0]);
+        	    			strco[i+1] = Double.parseDouble(strr[1]);
+        	    		}
+        	    		else{
+        	    			String [] strr = str[i].split("-");
+        	    			strex[i] = Integer.parseInt(strr[0]);
+        	    			strco[i+1] = Double.parseDouble("-"+strr[1]);
+        	    		}
+        	    	}
+        	    	strex[a-1] = Integer.parseInt(str[a-1]);
+        	    }
+        	    else {
+        	    	for(int i=1; i<a-1; i++) {
+        	    		if(str[i].indexOf('+')!=-1) {
+        	    			String [] strr = str[i].split("\\+");
+        	    			strex[i-1] = Integer.parseInt(strr[0]);
+        	    			strco[i] = Double.parseDouble(strr[1]);
+        	    		}
+        	    		else{
+        	    			String [] strr = str[i].split("-");
+        	    			strex[i-1] = Integer.parseInt(strr[0]);
+        	    			strco[i] = Double.parseDouble("-"+strr[1]);
+        	    		}
+
+        	    	}
+        	    	strex[a-2] = Integer.parseInt(str[a-1]);
+        	    }
+        	    co = strco;
+        	    ex = strex;
+        	}
+        	input.close();
+        	System.out.println("Successfully convey!");
+    	}
+    	catch(FileNotFoundException e) {
+    		System.out.println("Error!");
+    		e.printStackTrace();
+    	}
+    }
+
+    public Polynomial add(Polynomial b){
+    	int al = ex.length;
+    	int bl = (b.ex).length;
+    	int cs;
+    	if(ex[al-1] >= b.ex[bl-1]) {
+    		cs = ex[al-1]+1;
+    	}
+    	else {
+    		cs = b.ex[bl-1]+1;
+    	}
+    	
+    	double [] copy = new double[cs];
+    	
+    	for(int i=0; i<al; i++){
+    		copy[ex[i]] += co[i];
+    	}
+
+    	for(int j=0; j<bl; j++){
+    		copy[b.ex[j]] += b.co[j];
+    	}
+    	
+    	int count=0;
+    	for(int k = 0; k<cs; k++) {
+    		if(copy[k]!=0) count++;
+    	}
+    	double [] copyco = new double[count];
+    	int [] copyex = new int[count];
+    	
+    	int n = 0;
+    	for(int m=0; m<cs;m++) {
+    		if(copy[m]!=0) {
+    			copyco[n] = copy[m];
+    			copyex[n] = m;
+    			n++;
+    		}
+    	}
+    	
+    	Polynomial result = new Polynomial(copyco, copyex);
+    	return result;
+    }
+    
     public double evaluate(double x) {
-    	if (arr == null) {
+    	if (ex == null) {
     		return 0;
     	}
     	
     	double sum = 0;
-    	for(int i=0; i<arr.length; i++) {
-    		sum = sum + arr[i] * Math.pow(x, i);
+    	for(int i=0; i<ex.length; i++) {
+    		sum = sum + co[i] * Math.pow(x, ex[i]);
     	}
     	return sum;
     }
@@ -41,5 +147,71 @@ public class Polynomial{
     public boolean hasRoot(double r) {
     	double result = this.evaluate(r);
     	return (result == 0);
+    }
+    
+    public Polynomial multiply(Polynomial b) {
+    	int al = ex.length;
+    	int bl = (b.ex).length;
+    	int cs;
+    	cs = ex[al-1]+b.ex[bl-1]+1;
+    	
+    	double [] copy = new double[cs];
+    	for(int i=0; i<al; i++) {
+    		for(int j=0; j<bl; j++) {
+    			copy[ex[i]+b.ex[j]] += co[i] * b.co[j];
+    		}
+    	}
+    	
+    	int count=0;
+    	for(int k = 0; k<cs; k++) {
+    		if(copy[k]!=0) count++;
+    	}
+    	double [] copyco = new double[count];
+    	int [] copyex = new int[count];
+    	
+    	int n = 0;
+    	for(int m=0; m<cs;m++) {
+    		if(copy[m]!=0) {
+    			copyco[n] = copy[m];
+    			copyex[n] = m;
+    			n++;
+    		}
+    	}
+    	
+    	Polynomial result = new Polynomial(copyco, copyex);
+    	return result;
+    	
+    }
+    public void saveToFile(String ad) {
+    	String s;
+    	if(ex[0]==0) {
+    		s = Double.toString(co[0]);
+    	}
+    	else {
+    		s = Double.toString(co[0]) + "X" + Integer.toString(ex[0]);
+    	}
+    	for (int i=1; i<ex.length; i++) {
+    		if(co[i]>0) {
+    			s = s + "+" + Double.toString(co[i]) + "X" + Integer.toString(ex[i]);
+    		}
+    		else {
+    			s = s + Double.toString(co[i]) + "X" + Integer.toString(ex[i]);
+    		}
+    	}
+    	try {
+    		FileWriter mywriter = new FileWriter(ad);
+    		mywriter.write(s);
+    		mywriter.close();
+    		System.out.println("Successfully write!");
+    	}
+    	catch(IOException e) {
+    		System.out.println("Error!");
+    		e.printStackTrace();
+    	}
+    }
+    public void display() {
+    	for(int i=0; i<ex.length; i++) {
+    		System.out.println(co[i]);
+    	}
     }
 }
